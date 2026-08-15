67c6fa96ace5f9caf7eac61e2c29ca577adfa492
adoptvalue12 <adoptmegiverpet@gmail.com>
2026-08-15 04:54:34 +0200

    Update kevinduels.lua

--- kevinduels.lua
+++ kevinduels.lua
@@ -1,8 +1,3 @@
-do
-  local lfwm_7e97626092df4313 = "LFWM1_XFYgxFhwlXshNr89c7mk9e7UnYTV0AVw"
-  if false then error(lfwm_7e97626092df4313) end
-end
-
 local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
 local UIS = game:GetService("UserInputService")
